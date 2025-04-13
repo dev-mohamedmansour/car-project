876b1f6e938193609c667a7ffea176c201f7efb1
mohamed mansour <mohamedmansour0037@gmail.com>
2025-04-13 19:24:09 +0200

    Update database credentials
    Changes database connection details from production server to local development environment.

--- Logic/Models/Db.php
+++ Logic/Models/Db.php
@@ -11,8 +11,7 @@
 			 public function __construct()
 			 {
 					$this->connection = mysqli_connect(
-						 "sql211.infinityfree.com", "if0_38609934", "2fGQPifltlr6",
-						 "if0_38609934_car_house"
+						 "localhost", "root", "2772003", "car_house"
 					);
 					
 			 }
