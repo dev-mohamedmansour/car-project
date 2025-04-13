69f55137c12bfbc03cd457f4cf9e239a73d55bce
mohamed mansour <mohamedmansour0037@gmail.com>
2025-04-13 19:08:59 +0200

    Update database connection credentials
    Use production database credentials instead of local development credentials.

--- Logic/Models/Db.php
+++ Logic/Models/Db.php
@@ -10,12 +10,11 @@
 			 
 			 public function __construct()
 			 {
-//					$this->connection = mysqli_connect(
-//						 "sql211.infinityfree.com", "if0_38609934", "2fGQPifltlr6", "if0_38609934_car_house"
-//					);
 					$this->connection = mysqli_connect(
-						 "localhost", "root", "2772003", "car_house"
+						 "sql211.infinityfree.com", "if0_38609934", "2fGQPifltlr6",
+						 "if0_38609934_car_house"
 					);
+					
 			 }
 			 
 			 public function select($column, $table)
